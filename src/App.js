1fc3201939e99d1dfeb041fc71cdacd7900e83d5
Omer Sozcu <omerfsozcu@gmail.com>
2022-09-25 20:22:49 +0200

    addedd

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
       <Title />
       <MenuItems />
       <Title />
+      <Title />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
